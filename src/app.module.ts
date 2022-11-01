1df8021c2e6edbd6090dcbc90eb383bda4dfc851
jesusgm0 <gabrielmarioj1@gmail.com>
2022-11-01 09:56:28 -0300

    DeployblogPessoal

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,15 +12,26 @@ import { UsuarioModule } from "./usuario/usuario.module";
 
 @Module({
   imports: [
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: 'localhost',
+    //   port: 3306,
+    //   username: 'root',
+    //   password: 'rootroot',
+    //   database: 'db_blogpes3',
+    //   entities: [Postagem, Tema, Usuario],
+    //   synchronize: true
+    // }),
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'rootroot',
-      database: 'db_blogpes3',
-      entities: [Postagem, Tema, Usuario],
-      synchronize: true
+      type: 'postgres',
+      url: process.env.DATABASE_URL,          
+      logging: false, 
+      dropSchema:false,
+      ssl:{
+        rejectUnauthorized:false  
+      },
+      synchronize: true,
+      autoLoadEntities: true,
     }),
       PostagemModule,
       TemaModule,
